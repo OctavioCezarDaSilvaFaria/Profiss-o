faa631b37afb4129529996bfe9111d3141dc24cc
OctavioCezarDaSilvaFaria <tavinhojabuca@gmail.com>
2024-08-01 11:19:42 -0300

    neurologista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import Neuro from './Neuro.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div id="all">
+      <header>
+        <p id="title">
+          Neurocirurgião
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div id="container">
+      <div id="img">
+      <img src={Neuro} />
+      </div>
+        <br/>
+        <br/>
+        <div class="text">
+          <h2>Introdução:</h2>
+          <p>
+            Neurocirurgião é o médico especialista em cirurgia do sistema nervoso central e periférico, que abrangem o cérebro,
+            a coluna e todas as ramificações nervosas responsáveis pelos cinco sentidos e por todos os movimentos do corpo.
+            E um neurocirurgião pode tratar traumatismos cranianos, aneurismas, malformações,
+            tumores do encéfalo e da caixa óssea craniana, tumores da coluna; hidrocefalia, edema cerebral,
+            deformações do crânio congênitas, afundamentos e falhas ósseas; dores crônicas com procedimentos cirúrgicos, entre outros.
+          </p>
+        </div>
+        <br/>
+        <div class="text">
+          <h2>Habilidades Necessárias:</h2>
+
+          <li>Empatia</li>
+          <li>Liderança</li>
+          <li>Pensamento Crítico</li>
+          <li>Concentração</li>
+          <li>Mãos Precisas</li>
+          <li>Educação Continuada</li>
+          <li>Organização</li>
+          <li>Bom Gerenciamento do Tempo</li>
+        </div>
+        <br/>
+        <div class="text">
+          <h2>Oportunidades de Carreira:</h2>
+          <li>Cardiocirurgião</li>
+          <li>Cirurgião Torácico</li>
+          <li>Ortopedista</li>
+        </div>
+        <br/>
+        <div class="text">
+          <h2>Ferramentas e Tecnologias Usadas:</h2>
+          <li>IoT Médico</li>
+          <li>Telemedicina</li>
+          <li>Wearables</li>
+          <li>IA</li>
+          <li>Bioimpressão 3D</li>
+          <li>Aplicativos Méciso</li>
+          <li>Prontuário Eletrônico</li>
+          <li>Microscópio de Alta Resolução</li>
+          <li>Telemedicina</li>
+          <li>BIP</li>
+          <li>Utensílios Médicos</li>
+        </div>
+        <br/>
+        </div>
+      <footer>
+        <p>Todos os direitos reservados © 2024</p>
+      </footer>
     </div>
+
+
   );
 }
 
